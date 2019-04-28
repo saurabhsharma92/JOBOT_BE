fd390567ec1580ff008e0d636e433dd3c62ae23a
Saurabh Sharma <43507969+saurabhsharma92@users.noreply.github.com>
2019-04-27 21:44:01 -0400

    Initial Commit - Spring Hibernate Setup

---
+++ jobot/src/main/java/com/project/jobot/config/WebConfig.java
@@ -0,0 +1,13 @@
+package com.project.jobot.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {"com.project.jobot.controller"})
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+}
